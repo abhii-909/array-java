139446ae52352b3893a02ef0d10c43a7d8cc985f
Abbhinav Mishra <148212887+abhii-909@users.noreply.github.com>
2024-09-18 18:13:54 +0530

    Time: O(n)  |  Space: O(1)

--- checkSortedArray.java
+++ checkSortedArray.java
@@ -3,7 +3,7 @@
 
 import java.util.*;
 
-public class sortedArray {
+public class checkSortedArray {
     public static boolean checkSort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             if(arr[i] >= arr[i+1]){
